9d2f46b99516b48fe5c8ba18da2a7f48bf618425
wallyson ruan <wallyson.ruan@thoughtworks.com>
2023-03-16 19:10:59 -0300

    WIP: Added the New Entry button
    - Grouped the table rows in HEADER and BODY;
    - Added a button to create new entry rows, but its lacking the ability to sum.

--- script.js
+++ script.js
@@ -47,10 +47,60 @@ function updateResultTotal() {
     resultAll.value = finalResult;
 }
 
+function newEntryRow() {
+    let table = document.querySelector("tbody");
+
+    let row = document.createElement("tr");
+    let columnCheckIn = document.createElement("td");
+    let columnCheckOut = document.createElement("td");
+    let columnLunch = document.createElement("td");
+    let columnResultUnit = document.createElement("td");
+
+    let inputCheckIn = createInputCheck("in");
+    let inputCheckOut = createInputCheck("out");
+    let inputLunchCheckbox = document.createElement("input");
+    inputLunchCheckbox.className = "input--checkbox";
+    inputLunchCheckbox.id = "lunch_checkbox_" + getTotalOfRowsPlusOne();
+    inputLunchCheckbox.checked = true;
+    inputLunchCheckbox.type = "checkbox";
+    let inputResultUnit = document.createElement("input");
+    inputResultUnit.classList.add("input--text", "result", "result__unit");
+    inputResultUnit.id = "result_unit_" + getTotalOfRowsPlusOne();
+    inputResultUnit.readOnly = true;
+    inputResultUnit.type = "text";
+
+    columnCheckIn.appendChild(inputCheckIn)
+    columnCheckOut.appendChild(inputCheckOut);
+    columnLunch.appendChild(inputLunchCheckbox);
+    columnResultUnit.appendChild(inputResultUnit);
+
+    row.appendChild(columnCheckIn);
+    row.appendChild(columnCheckOut);
+    row.appendChild(columnLunch);
+    row.appendChild(columnResultUnit);
+    row.className = "entry-row";
+
+    table.appendChild(row);
+}
+
+function createInputCheck(inOrOut) {
+    let inputCheck = document.createElement("input");
+    inputCheck.type = "time";
+    inputCheck.required = "required";
+    inputCheck.className = "input--time";
+    inputCheck.id = "check" + inOrOut + "_time_" + getTotalOfRowsPlusOne();
+
+    return inputCheck;
+}
+
+function getTotalOfRowsPlusOne() {
+    let table = document.querySelectorAll(".entry-row");
+    return table.length + 1;
+}
 
 function copyResultTotal() {
     var resultTime = document.querySelector("#result__total");
     resultTime.select();
     navigator.clipboard.writeText(resultTime.value);
-    alert("The value: "+ resultTime.value + " has been copied to the clipboard.");
+    alert("The value: " + resultTime.value + " has been copied to the clipboard.");
 }

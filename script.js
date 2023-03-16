6aecb0a272b54368eaf4f3be54d35b898ad828bd
wallyson ruan <wallyson.ruan@thoughtworks.com>
2023-03-16 13:14:12 -0300

    FEAT: each row has its own time sum

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-function updateResult() {
-    var resultTime = document.getElementById('result_time');
+function updateFinalResult() {
+    let results = document.querySelectorAll(".updateFinalResult");
+    let finalResult = 0;
+    let resultAll = document.querySelector(".result--all");
 
-    var time = calculateTime();
-    if (time) {
-        resultTime.value = time;
-    }
+    results.forEach((result) => {
+        finalResult += result.value;
+    });
+
+    resultAll.value = finalResult;
 }
 
 function copyResult() {
@@ -12,6 +15,36 @@ function copyResult() {
     resultTime.select();
     document.execCommand("Copy");
 }
+
+function calculateTimeRuan(checkInId, checkOutId, lunchCheckboxId) {
+    var checkin = document.getElementById(checkInId).value;
+    var checkout = document.getElementById(checkOutId).value;
+
+    if (!checkin || !checkout) {
+        return null;
+    }
+
+    checkin = convertToDecimal(checkin);
+    checkout = convertToDecimal(checkout);
+    var lunch = document.getElementById(lunchCheckboxId).checked ? 1 : 0;
+
+    var result = checkout - checkin - lunch;
+    if (result < 0) {
+        result = 0;
+    }
+
+    return result.toFixed(2);
+}
+
+function updateResultRuan(checkInId, checkOutId, lunchCheckboxId, resultContainerId) {
+    var resultTime = document.getElementById(resultContainerId);
+
+    var time = calculateTimeRuan(checkInId, checkOutId, lunchCheckboxId);
+    if (time) {
+        resultTime.value = time;
+    }
+}
+
 
 function calculateTime() {
     var checkin = document.getElementById('checkin_time').value;
